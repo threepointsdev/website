cbed67cb3f28802eb13f3f0f6daa1511cdbdad9a
Andrea Poggi <apoggi@imolainformatica.it>
2024-10-17 16:10:12 +0200

    Introdotta pagina caircamento

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // script.js
+window.addEventListener('load', function () {
+    // Nascondi la schermata di caricamento con un effetto di dissolvenza
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.opacity = '0';
+
+    // Aspetta la fine della transizione prima di nascondere l'elemento dal flusso del layout
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+
+        // Mostra il contenuto del sito con un effetto di dissolvenza
+        const siteContent = document.getElementById('site-content');
+        siteContent.style.opacity = '1';
+    }, 200); // Tempo corrispondente alla durata della transizione in CSS (0.2s)
+});
+
+// script.js
 document.addEventListener("DOMContentLoaded", function () {
 
     const menuToggle = document.getElementById("menu-toggle");
